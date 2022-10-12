7e387026e1602269d1c7a85b0e092cf0d2213986
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-10-12 09:44:00 -0500

    Add queries with redux toolkit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,21 +1,46 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import rapidApiKey from "../../keyRapidAPI";
 
 export const shazamCoreApi = createApi({
 	reducerPath: "shazamCoreApi",
 	baseQuery: fetchBaseQuery({
 		baseUrl: "https://shazam-core.p.rapidapi.com/v1",
 		prepareHeaders: headers => {
-			headers.set(
-				"X-RapidAPI-Key",
-				"80a4036851mshf957a27b7005395p14977ejsnfbf7a8895cd6"
-			);
+			headers.set("X-RapidAPI-Key", rapidApiKey);
 
 			return headers;
 		}
 	}),
 	endpoints: builder => ({
-		getTopCharts: builder.query({ query: () => "/charts/world" })
+		getTopCharts: builder.query({ query: () => "/charts/world" }),
+		getSongsByGenre: builder.query({
+			query: genre => `/charts/genre-world?genre_code=${genre}`
+		}),
+		getSongsByCountry: builder.query({
+			query: countryCode => `/charts/country?country_code=${countryCode}`
+		}),
+		getSongsBySearch: builder.query({
+			query: searchTerm =>
+				`/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`
+		}),
+		getArtistDetails: builder.query({
+			query: artistId => `/artists/details?artist_id=${artistId}`
+		}),
+		getSongDetails: builder.query({
+			query: ({ songid }) => `/tracks/details?track_id=${songid}`
+		}),
+		getSongRelated: builder.query({
+			query: ({ songid }) => `/tracks/related?track_id=${songid}`
+		})
 	})
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const {
+	useGetTopChartsQuery,
+	useGetSongsByGenreQuery,
+	useGetSongsByCountryQuery,
+	useGetSongsBySearchQuery,
+	useGetArtistDetailsQuery,
+	useGetSongDetailsQuery,
+	useGetSongRelatedQuery
+} = shazamCoreApi;
